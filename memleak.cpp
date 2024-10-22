ea7cb0a0283fc9c0b3945c565192cb8bbc39d75a
saiakash1234 <48388678+saiakash1234@users.noreply.github.com>
2024-10-22 11:57:26 +0530

    Update memleak.cpp delete linked list

--- memleak.cpp
+++ memleak.cpp
@@ -20,9 +20,47 @@ void createArray() {
 
     // Since we are demonstrating a memory leak, we will omit the delete[] statement.
 }
+struct node
+{
+ int data;
+ node* link;
+};
+node * delete_linked_list(node *head)
+{
+    while(head!=nullptr)
+        {
+            node * temp = head;
+            head=head->link;
+        }
+        return head;
+}
 
 int main() {
     createArray();
+    node * temp1 = new node;
+    temp1->data = 5;
+    temp1->link = nullptr;
+
+node * temp2 = new node;
+    temp2->data = 6;
+    temp2->link = nullptr;
+
+node * temp3 = new node;
+    temp3->data = 7;
+    temp3->link = nullptr;
+
+node * temp4 = new node;
+    temp4->data = 8;
+    temp4->link = nullptr;
+
+node * head = temp1;
+
+temp1->link = temp2;
+temp2->link = temp3;
+temp3->link = temp4;
+
+head = delete_linked_list(node *head);
+
 
     std::cout << "Array created, initialized, printed, but not freed." << std::endl;
 
